91bac8a16e0707d8810ea638def29fbe87ad7108
VladBoyko11 <vlboyko11@gmail.com>
2022-12-07 11:40:00 +0200

    CourseWork

--- Coursework/trapazoidalDestribution.rb
+++ Coursework/trapazoidalDestribution.rb
@@ -1,12 +1,12 @@
 require 'benchmark'
 
-@a = 0
-@b = 0.9
-@c = 1.5
-@d = 2.1
+$a = 0
+$b = 0.9
+$c = 1.5
+$d = 2.1
 
-@step = 0.0001
-@quantity = 10_000
+$step = 0.0001
+$quantity = 10_000
 
 def trapezoidal_distribution (x, a, b, c, d)
   if x >= a && x < b
@@ -20,8 +20,8 @@ end
 
 def method_neyman (a, b, c ,d)
   @W = 0
-  (1..@quantity).each { |i|
-    @Ti = trapezoidal_distribution(i / @quantity, a, b, c ,d)
+  (1..$quantity).each { |i|
+    @Ti = trapezoidal_distribution(i / $quantity, a, b, c ,d)
     if @Ti > @W
       @W = @Ti
     end
@@ -38,7 +38,7 @@ def method_neyman (a, b, c ,d)
 end
 
 def method_metropolis(a, b, c, d)
-  (1..@quantity).each { |i|
+  (1..$quantity).each { |i|
     @x0 = 0.5
     @del = 0.2
     @x = @x0 + (-1 + 2 * rand) * @del
@@ -64,10 +64,10 @@ end
 
 def simplest_monte_carlo (a, b, c, d)
   sum = 0
-  (1...@quantity).each {
+  (1...$quantity).each {
     sum += trapezoidal_distribution(rand(a.to_f...d.to_f), a, b, c, d)
   }
-  sum / @quantity
+  sum / $quantity
 end
 
 # def resMethodNeyman2 (x)
@@ -93,29 +93,29 @@ end
 # @max = 1
 
 
-res_method_neyman = method_neyman(@a, @b, @c, @d)
-res_method_metropolis = method_metropolis(@a, @b, @c, @d)
-res_method_inverse_function = method_inverse_function(@a, @b, @c, @d)
-res_simplest_monte_carlo = simplest_monte_carlo(@a, @b, @c, @d)
+res_method_neyman = method_neyman($a, $b, $c, $d)
+res_method_metropolis = method_metropolis($a, $b, $c, $d)
+res_method_inverse_function = method_inverse_function($a, $b, $c, $d)
+res_simplest_monte_carlo = simplest_monte_carlo($a, $b, $c, $d)
 
 file = File.new("#{__dir__}/file.txt", "a:UTF-8")
 file.print("Умови інтегрування наступні: \n")
-file.print("Область інтегрування: #{@a} <= #{@b} <= #{@c} <= #{@d} \n")
-file.print("Кількість обчислень для статистичних методів: #{@quantity} \n")
+file.print("Область інтегрування: #{$a} <= #{$b} <= #{$c} <= #{$d} \n")
+file.print("Кількість обчислень для статистичних методів: #{$quantity} \n")
 file.print("Метод Неймана: #{res_method_neyman}\n")
 file.print("Метод Метрополісу: #{res_method_metropolis}\n")
 file.print("Метод зворотної функції: #{res_method_inverse_function}\n")
 file.print("Найпростіший метод Монте Карло: #{res_simplest_monte_carlo}\n")
-file.print("Крок для детерміністичних методів: #{@step}\n")
-file.print("Час обчислення (1): #{method_neyman(@a, @b, @c, @d).real} c\n")
-file.print("Час обчислення (2): #{method_metropolis(@a, @b, @c, @d).real} c\n")
-file.print("Час обчислення (3): #{method_inverse_function(@a, @b, @c, @d).real} c\n")
-file.print("Час обчислення (4): #{simplest_monte_carlo(@a, @b, @c, @d).real} c\n")
+file.print("Крок для детерміністичних методів: #{$step}\n")
+file.print("Час обчислення (1): #{method_neyman($a, $b, $c, $d).real} c\n")
+file.print("Час обчислення (2): #{method_metropolis($a, $b, $c, $d).real} c\n")
+file.print("Час обчислення (3): #{method_inverse_function($a, $b, $c, $d).real} c\n")
+file.print("Час обчислення (4): #{simplest_monte_carlo($a, $b, $c, $d).real} c\n")
 file.close
 
 puts "Умови інтегрування наступні: \n
-Область інтегрування: #{@a} <= #{@b} <= #{@c} <= #{@d}
-Кількість обчислень для статистичних методів: #{@quantity}"
+Область інтегрування: #{$a} <= #{$b} <= #{$c} <= #{$d}
+Кількість обчислень для статистичних методів: #{$quantity}"
 
 
 puts "Метод Неймана: #{res_method_neyman}"
@@ -123,10 +123,10 @@ puts "Метод Метрополісу: #{res_method_metropolis}"
 puts "Метод зворотної функції: #{res_method_inverse_function}"
 puts "Найпростіший метод Монте Карло: #{res_simplest_monte_carlo}"
 
-puts "Крок для детерміністичних методів: #{@step}"
-puts "Час обчислення (1): #{method_neyman(@a, @b, @c, @d).real} c"
-puts "Час обчислення (2): #{method_metropolis(@a, @b, @c, @d).real} c"
-puts "Час обчислення (3): #{method_inverse_function(@a, @b, @c, @d).real} c"
-puts "Час обчислення (4): #{simplest_monte_carlo(@a, @b, @c, @d).real} c"
+puts "Крок для детерміністичних методів: #{$step}"
+puts "Час обчислення (1): #{method_neyman($a, $b, $c, $d).real} c"
+puts "Час обчислення (2): #{method_metropolis($a, $b, $c, $d).real} c"
+puts "Час обчислення (3): #{method_inverse_function($a, $b, $c, $d).real} c"
+puts "Час обчислення (4): #{simplest_monte_carlo($a, $b, $c, $d).real} c"
 
 
